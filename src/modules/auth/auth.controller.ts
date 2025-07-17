402ccbabbec5f238e27407a6676dfbb60a6667eb
Emmanuel Omoiya <emmanuelomoiya6@gmail.com>
2025-07-17 16:05:32 +0100

    feat: add automatic cookie storage during authentication

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, UseGuards, Req, Post, Body, Param, Query, Put, Delete, Patch } from "@nestjs/common";
+import { Controller, Get, UseGuards, Req, Post, Body, Param, Query, Put, Delete, Patch, Res } from "@nestjs/common";
 import { ApiBadRequestResponse, ApiBearerAuth, ApiBody, ApiCreatedResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { AuthService } from "./auth.service";
 import { AuthResponse, BadRequestResponse, LoginDto, RegisterDto, ResetPasswordDTO } from "./dtos";
 import { AccessTokenGuard } from "./guards/jwt.guard";
 import { RefreshTokenGuard } from "./guards/refreshToken.guard";
 import { IResponse, TokenData } from "@//interfaces";
+import { Response } from "express";
 
 @ApiTags('auth')
 @Controller({ version: '1', path: 'auth' })
@@ -15,22 +16,24 @@ export class AuthController {
     @ApiOperation({ summary: 'Endpoint to create an account' })
     @ApiCreatedResponse({ type: AuthResponse })
     @ApiBadRequestResponse({ type: BadRequestResponse })
-    async register(@Body() body: RegisterDto) {
-        return await this.authService.register(body);
+    async register(@Body() body: RegisterDto, @Res({ passthrough: true }) res: Response) {
+        return await this.authService.register(body, res);
     }
 
     @Post('login')
     @ApiOperation({ summary: 'Endpoint to login to your account' })
-    async login(@Body() payload: LoginDto) {
-        return await this.authService.login(payload);
+    async login(@Body() payload: LoginDto, @Res({ passthrough: true }) res: Response) {
+        return await this.authService.login(payload, res);
     }
 
     @Post('logout')
     @UseGuards(AccessTokenGuard)
     @ApiBearerAuth()
     @ApiOperation({ summary: 'Endpoint to logut from your account' })
-    async logout(@Req() auth: Request & { user: TokenData }) {
+    async logout(@Req() auth: Request & { user: TokenData }, @Res({ passthrough: true }) res: Response) {
         const { user } = auth;
+        res.clearCookie('accessToken');
+        res.clearCookie('refreshToken');
         return await this.authService.logout(user as unknown as TokenData);
     }
 
